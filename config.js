74a013671dc8691845c31c99a416fa5a7a062ef3
hongdangcseiu <hongdangcseiu@gmail.com>
2024-03-07 01:14:23 +0700

    config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   db: {
     database_host_name: "localhost",
-    database_user_name: "root",
-    database_password: "",
-    database_name: "test",
+    database_user_name: "sql_rapphim_vn",
+    database_password: "x3ShHsp8LPTxJKy8",
+    database_name: "sql_rapphim_vn",
   },
   gal: {
     gal_client_id: "3da4eba4-94dd-4e74-bc25-38e89a01fe07",
