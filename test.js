aeab6e743e4e7c57458ea41fef00e6ba33fd1354
Pham Hong Dang <hongdang2k1@gmail.com>
2024-10-30 04:51:14 +0700

    test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const savePoster = require('./download-image');
-savePoster("https://cdn.galaxycine.vn/media/2024/3/12/exhuma-500_1710214991225.jpg", "C:/Users/Pham D/Documents/temp/ihello.png" ).then(() => {
+savePoster("https://cdn.galaxycine.vn/media/2024/3/12/exhuma-500_1710214991225.jpg", "ihello.png" ).then(() => {
     console.log("Done");
 }   ).catch((err) => {
     console.log(err);
